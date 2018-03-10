f0b5e2f75667fafefbbb3093ec1930a68f2e400c
jaimemendozadev <jaimemendozadev@gmail.com>
2018-03-10 13:29:51 -0800

    Refactor routes to send text messages

--- server/router/index.js
+++ server/router/index.js
@@ -1,9 +1,32 @@
 const Router = require('express').Router();
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
+const {makeACall} = require('../utils.js');
 
 Router.get('/', (req, res) => {
   res.send('hit the api bruh');
 });
+
+Router.post('/orderresponse', makeACall);
+
+
+Router.post('/order', (req, res) => {
+ 
+  // Start our TwiML response.
+  const twiml = new MessagingResponse();
+
+  console.log("twiml ", twiml);
+ 
+  // Add a text message.
+  const msg = twiml.message('Check out this dancing hot coffee!');
+ 
+  // Add a picture message.
+  msg.media('https://i.pinimg.com/originals/8c/6c/55/8c6c55cd70c5dc9bac7860b8d89c386c.gif');
+
+  console.log("msg ", msg);
+ 
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
 
 module.exports = Router;
---
+++ server/utils.js
@@ -0,0 +1,27 @@
+//Twilio Credentials
+const accountSid = process.env.ACCOUNTSID;
+const authToken = process.env.AUTHTOKEN;
+
+//require the Twilio module and create a REST client
+const client = require('twilio')(accountSid, authToken);
+
+
+const makeACall = (req, res) => {
+
+  client.messages
+    .create({
+      to: '+13233837587',
+      from: process.env.PHONENUMBER,
+      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+    })
+    .then(message => {
+      console.log("Message ", message);
+      console.log("message.sid ", message.sid);
+      
+      res.send(message);
+    });
+}
+
+module.exports = {
+  makeACall
+}
