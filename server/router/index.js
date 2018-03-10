92b1f1619d89cb8b9aaa925d645d822a2f0f640f
jaimemendozadev <jaimemendozadev@gmail.com>
2018-03-10 10:14:36 -0800

    Add ngrok to .gitignore

--- server/router/index.js
+++ server/router/index.js
@@ -1,4 +1,6 @@
 const Router = require('express').Router();
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
 
 Router.get('/', (req, res) => {
   res.send('hit the api bruh');
