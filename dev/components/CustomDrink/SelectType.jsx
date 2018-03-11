99f430a10d00e5d0775424b64843a1d4603e54e3
jaimemendozadev <jaimemendozadev@gmail.com>
2018-03-11 09:09:55 -0700

    Fix setState issue in SelectType checkbox

--- dev/components/CustomDrink/SelectType.jsx
+++ dev/components/CustomDrink/SelectType.jsx
@@ -3,14 +3,60 @@ import {connect} from 'react-redux';
 
 
 class SelectionType extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        "Iced": {
+          "Latte": false  
+        },
+
+        "Hot": {
+    
+        }
+      }
+      this.handleInputChange = this.handleInputChange.bind(this);
+      
+  }
+    
+  
+
+  handleInputChange(event, temperature) {
+
+    //value is boolean that checks if input is clicked
+    const value = event.target.checked;
+
+    //get name attr from <input>
+    const name = event.target.name;
+
+    //make copy of old state
+    const newState = {...this.state};
+
+    //update state
+    newState[temperature][name] = value
+
+    this.setState(newState);
+  }
   
   render() {
+    console.log("selectType State ", this.state)
     return (
       <div>
         <h1>What cup o' Joe would you like?</h1>
         <form>
           <h1>Iced</h1>
-          <h2>Hot</h2>
+
+          <label>Latte
+          <input
+            temperature="Iced"
+            type="checkbox"
+            name="Latte"
+            checked={this.state.icedLatte}
+            onClick={(event) => this.handleInputChange(event, "Iced")} />
+            </label> 
+            
+            
+            
+
         </form>
       </div>
     )
