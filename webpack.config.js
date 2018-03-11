bb478dae6d9c9aa1361d475c733bb89cf1fd358c
jaimemendozadev <jaimemendozadev@gmail.com>
2018-03-10 20:05:59 -0800

    Add extract-text-webpack-plugin to webpack
    Stylesheet loads from external file instead
    of injecting css in style tag in head tag.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,31 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const public = path.resolve(__dirname, 'public');
 const dev = path.join(__dirname + '/dev/index.jsx');
 
 module.exports = {
   entry: dev,
+
   output: {
     path: public,
     filename: 'bundle.js'
   },
+
   module: {
     rules: [
       { test: /\.js?x$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+      // { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+      { test: /\.scss$/, use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'sass-loader']
+      })}
+
     ]
-  }
+  },
+
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+  ]
 }
+
+
