ad8955ac1add0da561c2910c8b93961778a63a16
jaimemendozadev <jaimemendozadev@gmail.com>
2018-03-15 20:21:14 -0700

    Fix Google Passport Auth issue
    Issue was related to using a button for
    attaching Redux action and making API
    call. Changed it to anchor tag and now
    the authentication works.

--- dev/actions/Authentication.js
+++ dev/actions/Authentication.js
@@ -14,26 +14,7 @@ export const googleLogin = () => {
         })
         .catch(error => {
           console.log("the error is ", error);
-        })
-    //   let API_OPTIONS = {
-    //     method: 'GET',
-    //     mode: 'no-cors'   
-    //   };
-      
-    //   fetch(API, API_OPTIONS)
-    //     .then(results => {
-    //       //parse JWT into json
-    //       return results.json();
-    //     })
-    //     .then(serverResults => {
-  
-    //       //save server results in local storage
-    //       //dispatch AUTHENTICATED action
-    //       console.log("serverResults are ", serverResults)
-    //     })
-    //     .catch(error => {
-    //       //dispatch NOT_AUTHENTICATED action
-    //       console.log("the error from the server ", error);
-    //     });
+        });
     }
+
 }
--- dev/components/Login.jsx
+++ dev/components/Login.jsx
@@ -5,10 +5,9 @@ import {googleLogin} from '../actions/Authentication.js';
 const Login = ({googleLogin}) => (
   <div>
     <h1>Login</h1>
-    <button onClick={googleLogin} className="loginBtn loginBtn--google">
-      Login with Google
-    </button>
-    
+    <p className="loginBtn loginBtn--google"><a className="google-anchor" href='http://localhost:3000/api/auth/google' onClick={googleLogin}>
+      Login with Google</a></p>
+
   </div>
 );
 
